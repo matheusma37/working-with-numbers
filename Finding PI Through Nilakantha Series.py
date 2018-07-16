4859408105f652eef34c0870b48da65ec1d53601
matheusma37 <mtsma37@gmail.com>
2018-07-16 14:55:10 -0300

    Update Finding PI Through Nilakantha Series.py

--- Finding PI Through Nilakantha Series.py
+++ Finding PI Through Nilakantha Series.py
@@ -1,21 +1,22 @@
-PI = 3
-signalExchanger = 1
-divider = 2
+if __name__ == '__main__':
+    PI = 3
+    signalExchanger = 1
+    divider = 2
 
-try:
-    precision = int(input('''Enter the precision for the calculation.
+    try:
+        precision = int(input('''Enter the precision for the calculation.
 The higher the input value, the closer to PI will be the result.
 Input: '''))
-    if precision < 1:
-        raise ValueError
-except:
-    print('Error! Please input a positive integer value higher than zero...')
-    input()
-    exit()
+        if precision < 1:
+            raise ValueError
+    except:
+        print('Error! Please input a positive integer value higher than zero...')
+        input()
+        exit()
 
-for x in range(1, precision+1):
-    PI += 4/(divider*(divider+1)*(divider+2)) * signalExchanger
-    divider += 2
-    signalExchanger *= -1
+    for x in range(1, precision+1):
+        PI += 4/(divider*(divider+1)*(divider+2)) * signalExchanger
+        divider += 2
+        signalExchanger *= -1
 
-print('The result of PI is: {}'.format(PI))
+    print('The result of PI is: {}'.format(PI))
