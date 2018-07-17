67b0b531f28a5f7de8117d55cffc3911fc27d20d
matheusma37 <mtsma37@gmail.com>
2018-07-17 17:21:53 -0300

    Question 1.4
    Next Prime Number

---
+++ Next Prime Number.py
@@ -0,0 +1,29 @@
+def isPrime(value):
+    for v in range(2, (value//2)+1):
+        if value % v == 0:
+            return False
+    else:
+        return True
+
+def nextPrime(value):
+    init = value+1
+    while True:
+        if isPrime(init):
+            return init
+        init += 1
+
+if __name__ == '__main__':
+    
+    prime = 2
+    primes = [prime]
+    wantAnother = True
+    
+    while wantAnother:
+        print('Primes: {}'.format(primes))
+        print('Do you want the next prime number? Y/N')
+        answer = input('R: ').strip().upper()
+        wantAnother = answer == 'Y'
+        prime = nextPrime(prime)
+        primes.append(prime)
+    
+    print('Program completed.')
