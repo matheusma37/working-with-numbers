e9ee712db2dfb278abfba9d74a62717ccde4313f
matheusma37 <mtsma37@gmail.com>
2018-07-18 01:00:58 -0300

    Question 1.5
    Find Cost of Tile to Cover W x H Floor

---
+++ Cost to Cover With Tile.py
@@ -0,0 +1,29 @@
+def inputValue(text):
+    value = -1.0
+    while value <= 0:
+        try:
+            value = float(input('Input a value (greater '+
+                                'than zero) for {}: '
+                                .format(text)
+                                )
+                          )
+        except:
+            print('Please input a valid value.')
+    return value
+
+if __name__ == '__main__':
+    floorHeight = inputValue('area height')
+    floorWidth = inputValue('area width')
+
+    tileHeight = inputValue('tile height')
+    tileWidth = inputValue('area width')
+    tileCost = inputValue('tile cost')
+
+    tileArea = tileHeight * tileWidth
+    floorArea = floorHeight * floorWidth
+
+    totalCost = (floorArea/tileArea) * tileCost
+
+    print('The total cost to cover the floor is $ {}.'
+          .format(totalCost)
+          )
